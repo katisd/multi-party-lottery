adb47dc285f8aa5440bbeddf6c0ad372d3e3469c
Katid Supawee <katidd17@gmail.com>
2024-03-24 18:38:22 +0700

    feat(lottery): add revealChoice & findWinner function * removePlayer: if input is out of range

--- lottery.sol
+++ lottery.sol
@@ -60,4 +60,106 @@ contract lottery is CommitReveal {
         players.push(Player(msg.sender, 0, false));
         commit(hashChoice);
     }
+
+    function removePlayer() internal {
+        bool checkPlayer;
+        uint256 playerIndex;
+        (checkPlayer, playerIndex) = isPlayer();
+        require(playerIndex < players.length && checkPlayer == true);
+        // move to last then pop last array
+        players[playerIndex] = players[players.length - 1];
+        players.pop();
+    }
+
+    function revealChoice(
+        uint256 ans,
+        string memory salt
+    ) public returns (string memory) {
+        // check time
+        require(
+            block.timestamp > deadlineCommit && block.timestamp < deadlineReveal
+        );
+        // check if the player is already in the game
+        bool checkPlayer;
+        uint256 playerIndex;
+        (checkPlayer, playerIndex) = isPlayer();
+        require(checkPlayer == true);
+        // Reveal
+        revealAnswer(ans, salt);
+        if (ans > 999) {
+            removePlayer();
+            return
+                "You are out of game, since your choice are out of range [0-999]";
+        } else {
+            players[playerIndex].choice = ans + 1;
+            players[playerIndex].isReveal = true;
+            return "Your answer are reveal successfully";
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function findWinner() public onlyOwner returns (string memory, uint) {
+        // check time
+        require(
+            block.timestamp > deadlineReveal &&
+                block.timestamp < deadlineFindWinner
+        );
+        bool isThereArePlayer = false;
+        uint playerNum = 0;
+        // find index of winner
+        uint256 winner;
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i].choice == 0) {
+                continue;
+            }
+            playerNum += 1;
+            if (isThereArePlayer == false) {
+                isThereArePlayer = true;
+                winner = players[i].choice - 1;
+                continue;
+            }
+            winner = winner & (players[i].choice - 1);
+        }
+        address payable ownerAddress = payable(owner);
+        if (isThereArePlayer) {
+            winner = uint(keccak256(abi.encodePacked(winner))) % playerNum;
+            // find player that's not cheat and in (winner) position
+            playerNum = 0;
+            for (uint256 i = 0; i < players.length; i++) {
+                if (players[i].choice == 0) {
+                    continue;
+                }
+                if (playerNum == winner) {
+                    winner = i;
+                    break;
+                }
+                playerNum += 1;
+            }
+            address payable winnerAccount = payable(players[winner].addr);
+            // winnerAccount.transfer(0.01*participants*0.98 ether);
+            uint reward = ((participants * 98) * 1e18) / 1e5;
+            uint ownerReward = ((participants * 2) * 1e18) / 1e5;
+
+            resetGame();
+
+            winnerAccount.transfer(reward);
+            ownerAddress.transfer(ownerReward);
+            return ("Winner is recieve", reward);
+        }
+        ownerAddress.transfer(address(this).balance);
+        resetGame();
+        return ("Owner reviev all", 0);
+    }
+
+    function resetGame() internal {
+        deadlineCommit = 0;
+        deadlineFindWinner = 0;
+        deadlineReveal = 0;
+        delete players;
+        participants = 0;
+    }
 }
