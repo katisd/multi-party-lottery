42eba65c2933e0dc2f94dda4dc6670e66cd268ea
Katid Supawee <katidd17@gmail.com>
2024-03-24 18:22:52 +0700

    feat(lottery): commit function that allow commit once in each game

--- lottery.sol
+++ lottery.sol
@@ -15,11 +15,49 @@ contract lottery is CommitReveal {
     uint256 T3;
     uint256 deadlineFindWinner = 0;
 
+    struct Player {
+        address addr;
+        uint256 choice;
+        bool isReveal;
+    }
+    Player[] private players;
+
+    uint256 participants;
+
     constructor(uint256 n, uint256 t1, uint256 t2, uint256 t3) {
         owner = msg.sender;
         N = n;
         T1 = t1;
         T2 = t2;
         T3 = t3;
+    }
+
+    function isPlayer() private view returns (bool ans, uint256 index) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i].addr == msg.sender) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
+
+    function commitChoice(bytes32 hashChoice) public payable {
+        require(msg.value == 0.001 ether);
+        // check if the player is already in the game
+        bool checkPlayer;
+        (checkPlayer, ) = isPlayer();
+        require(checkPlayer == false);
+        // If the deadline is not set, set the deadline
+        if (deadlineCommit == 0) {
+            deadlineCommit = block.timestamp + T1;
+            deadlineReveal = deadlineCommit + T2;
+            deadlineFindWinner = deadlineReveal + T3;
+        }
+        // check time
+        require(block.timestamp <= deadlineCommit);
+        // add player to the game
+        participants++;
+        players.push(Player(msg.sender, 0, false));
+        commit(hashChoice);
     }
 }
